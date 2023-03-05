c3abfbada9e67d30eb5ccf9b90d0bd580d1d8d6c
Michaela Meissner <m.p.meissner85@gmail.com>
2023-03-05 17:05:15 -0500

    Created GetSongs Component and Started Function to Get All Songs

---
+++ src/Components/GetSongs/GetSongs.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const GetSongs = (props) => {
+    return ( 
+        
+     );
+}
+ 
+export default GetSongs;
+
+
+
+// const [songs, setSongs] = useState([]);
+
+
+// useEffect(() => {
+//   getAllSongs();
+// }, []);
+
+// async function getAllSongs(){
+//   const response = await axios.get('http://127.0.0.1:8000/api/music/');
+//   console.log(response.data);
+//   setSongs(response.data)
+// }
+
+// <button onClick={() => getAllSongs()}>Get All Songs</button>
+
+
+
+
